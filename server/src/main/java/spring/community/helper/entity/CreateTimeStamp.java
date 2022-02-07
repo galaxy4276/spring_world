5191a1fd14bfeeaa57bc04d46edbf1df42ecc0e3
galaxy4276 <galaxyhi4276@gmail.com>
2022-02-07 20:59:27 +0900

    feat: created_at 필드 생성 임베디드 엔티티 작성

---
+++ server/src/main/java/spring/community/helper/entity/CreateTimeStamp.java
@@ -0,0 +1,19 @@
+package spring.community.helper.entity;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Embeddable
+public class CreateTimeStamp {
+  @Temporal(TemporalType.TIMESTAMP)
+  @Generated(GenerationTime.INSERT)
+  @Column(name = "created_at", updatable = false, insertable = false, nullable = false,
+          columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+  private Date createdAt;
+}
