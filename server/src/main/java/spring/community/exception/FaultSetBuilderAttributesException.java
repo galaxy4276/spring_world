e1bc54ae5fada9f23166981dad556b497dc35006
galaxy4276 <galaxyhi4276@gmail.com>
2022-02-08 15:02:15 +0900

    feat: 빌더용 예외처리 클래스 작성

---
+++ server/src/main/java/spring/community/exception/FaultSetBuilderAttributesException.java
@@ -0,0 +1,9 @@
+package spring.community.exception;
+
+public class FaultSetBuilderAttributesException extends Exception {
+
+  public FaultSetBuilderAttributesException() {
+    super("필드 설정이 잘못되었습니다.");
+  }
+
+}
