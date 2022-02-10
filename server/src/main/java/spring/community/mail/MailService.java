a7e800cbd27a08cdd6f6e175535ed361b8e67441
galaxy4276 <galaxyhi4276@gmail.com>
2022-02-11 00:01:37 +0900

    feat: 이메일 구현체와 서비스 초안 작성

---
+++ server/src/main/java/spring/community/mail/MailService.java
@@ -0,0 +1,14 @@
+package spring.community.mail;
+
+public interface MailService {
+
+  // 이메일을 보냅니다.
+  public void sendMail(MailDto sendInfo);
+
+  // 사용자 회원가입 인증을 위한 이메일을 보냅니다.
+  public void sendUserJoinVerificationMail();
+
+  // 사용자 패스워드 찾기 인증을 위한 이메일을 보냅니다.
+  public void sendUserOneTimePasswordVerificationMail();
+
+}
