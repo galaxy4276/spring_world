0031fe7043d6234265fa9436481151fdb02c34dd
galaxy <galaxyhi4276@gmail.com>
2022-02-15 10:38:39 +0000

    feat: 회원가입 인증코드메일 발송 메서드 작성

--- server/src/main/java/spring/community/mail/MailService.java
+++ server/src/main/java/spring/community/mail/MailService.java
@@ -6,7 +6,7 @@ public interface MailService {
   public void sendMail(MailDto sendInfo);
 
   // 사용자 회원가입 인증을 위한 이메일을 보냅니다.
-  public void sendUserJoinVerificationMail();
+  public void sendUserJoinVerificationMail(String key, String username, String email);
 
   // 사용자 패스워드 찾기 인증을 위한 이메일을 보냅니다.
   public void sendUserOneTimePasswordVerificationMail();
--- server/src/main/java/spring/community/mail/MailServiceImpl.java
+++ server/src/main/java/spring/community/mail/MailServiceImpl.java
@@ -1,15 +1,18 @@
 package spring.community.mail;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import spring.community.authentication.service.GenerateAuthKeyService;
 
 @Component
 public class MailServiceImpl implements MailService {
 
-  @Autowired
-  private JavaMailSender mailSender;
+  private final JavaMailSender mailSender;
+
+  public MailServiceImpl(GenerateAuthKeyService generateAuthKeyService, JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
 
   @Override
   public void sendMail(MailDto sendInfo) {
@@ -22,7 +25,18 @@ public class MailServiceImpl implements MailService {
   }
 
   @Override
-  public void sendUserJoinVerificationMail() {}
+  public void sendUserJoinVerificationMail(String token, String username, String email) {
+    SimpleMailMessage ctx = new SimpleMailMessage();
+    ctx.setTo(email);
+    ctx.setSubject("'이런 서버쟁이들' 에서 보내는 인증 코드입니다.");
+    ctx.setText(
+      "'이런 서버쟁이들' 서버 개발자들의 소통 서비스에 가입해주셔서 감사합니다.\n" +
+        "아래 URL 을 클릭하여 메일 인증을 수행해주세요." +
+        "url: " + "http://localhost/auth/signup/mail/verification/" +
+        token
+    );
+    mailSender.send(ctx);
+  }
 
   @Override
   public void sendUserOneTimePasswordVerificationMail() {}
