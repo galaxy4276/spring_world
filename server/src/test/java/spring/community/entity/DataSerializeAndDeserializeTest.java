ac8a2d2865f5aa7561d044a590705c005042d840
galaxy <galaxyhi4276@gmail.com>
2022-02-16 23:26:44 +0000

    test: Dto, Entity 간 데이터 직렬화/역직렬화 테스트 작성

--- server/src/test/java/spring/community/entity/DataSerializeAndDeserializeTest.java
+++ server/src/test/java/spring/community/entity/DataSerializeAndDeserializeTest.java
@@ -7,37 +7,41 @@ import org.junit.jupiter.api.Test;
 import spring.community.user.dto.UserDto;
 import spring.community.user.entity.User;
 
-@DisplayName("엔티티 빌더 생성 테스트")
-public class BuilderTest {
-  private UserDto userDto;
-  private User user;
+import java.util.function.Function;
 
-  @Test
-  void userDto가_정상적으로_생성된다() {
-    String 이게_사람이름이냐 = "엄준식";
-    UserDto userDto = UserDto.builder()
+@DisplayName("Dto와 Entity 간 Serialize/Deserialize 테스트")
+public class DataSerializeAndDeserializeTest {
+  private final String 이게_사람이름이냐 = "엄준식";
+
+  UserDto generateUserDto() {
+    return UserDto.builder()
       .setName(이게_사람이름이냐)
       .setPassword("@Umjunsik12")
       .setEmail("icantBelieve@howumispeople.name")
       .build();
-    this.userDto = userDto;
+  }
+
+  @Test
+  void userDto가_정상적으로_생성된다() {
+    UserDto userDto = generateUserDto();
     assertThat(userDto).isInstanceOf(UserDto.class);
     assertThat(userDto.getName()).isSameAs(이게_사람이름이냐);
   }
 
   @Test
   void userDto를_User엔티티로_성공적으로_변환한다() {
-    User user = userDto.toEntity();
-    this.user = user;
+    User user = generateUserDto().toEntity();
     assertThat(user).isInstanceOf(User.class);
     assertThat(user.getName()).isSameAs("엄준식");
   }
 
   @Test
   void User엔티티를_userDto로_성공적으로_변환한다() {
-     UserDto successfullyDto = user.toUserDto();
-     assertThat(successfullyDto).isInstanceOf(user.getClass());
-     assertThat(successfullyDto.getName()).isSameAs("엄준식");
+    Function<UserDto, User> generateUser = UserDto::toEntity;
+    User user = generateUser.apply(generateUserDto());
+    UserDto userDto = user.toUserDto();
+    assertThat(userDto).isInstanceOf(UserDto.class);
+    assertThat(userDto.getName()).isSameAs("엄준식");
   }
 
 }
