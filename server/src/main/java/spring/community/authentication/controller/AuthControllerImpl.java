01c788c74057f6c6117de012a5098edddc92d214
galaxy <galaxyhi4276@gmail.com>
2022-02-16 14:55:01 +0000

    refactor: 변경된 인터페이스 패키지 위치 반영

--- server/src/main/java/spring/community/authentication/controller/AuthControllerImpl.java
+++ server/src/main/java/spring/community/authentication/controller/AuthControllerImpl.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spring.community.authentication.dto.SignupRequestDto;
-import spring.community.authentication.service.AuthService;
+import spring.community.authentication.service.interfaces.AuthService;
 import javax.validation.Valid;
 
 
