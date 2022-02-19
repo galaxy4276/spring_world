5d57a809dc6a796dd7171b4f78e06901fadc4249
galaxy4276 <galaxyhi4276@gmail.com>
2022-02-19 07:51:50 +0000

    refactor: signup 로직 코드 리팩터링 (#27)
    * refactor: 사용자 서비스 인터페이스 구현
    
    * fix: 사용하지 않는 파일 제거
    
    * refactor: controller 에 변경된 서비스 이름 반영
    
    * feat: signup  코드 리팩터링

--- server/src/main/java/spring/community/authentication/service/AuthServiceImpl.java
+++ server/src/main/java/spring/community/authentication/service/AuthServiceImpl.java
@@ -1,47 +1,39 @@
 package spring.community.authentication.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import spring.community.authentication.dto.SignupRequestDto;
 import spring.community.authentication.entity.SignupVerification;
 import spring.community.authentication.exception.AlreadyExistsUserException;
-import spring.community.authentication.repository.SignupVerificationRepository;
 import spring.community.authentication.service.interfaces.AuthDataHelpService;
 import spring.community.authentication.service.interfaces.AuthService;
 import spring.community.authentication.service.interfaces.GenerateAuthKeyService;
 import spring.community.mail.MailService;
 import spring.community.user.entity.User;
 import spring.community.user.repository.dao.UserRepository;
-
-import java.util.Optional;
+import spring.community.user.service.interfaces.UserService;
 
 /**
  * @desc 인증의 메인 기능이 구현됩니다.
  */
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AuthServiceImpl implements AuthService {
 
   private final AuthDataHelpService authDataHelpService;
   private final UserRepository userRepository;
   private final MailService mailService;
-  private final SignupVerificationRepository signupVerificationRepository;
+  private final UserService userService;
   private final GenerateAuthKeyService generateAuthKeyService;
 
   @Override
   public void signup(SignupRequestDto signupRequestDto) {
-    Optional<User> user =
-      userRepository.findByEmail(signupRequestDto.getEmail());
-    System.out.println(user.toString());
-    if (user.isPresent()) {
-      System.out.println("이미 유저 있는데?");
-      sendSignupVerificationUrl(user.get());
-    } else {
-      User newUser = signupRequestDto.toUserEntity();
-      userRepository.save(newUser);
-      sendSignupVerificationUrl(newUser);
-      System.out.println("새 유저 정보 저장 완료");
-    }
+    User user =
+      userRepository.findByEmail(signupRequestDto.getEmail())
+          .orElseGet(() -> userService.createUserByRequestDto(signupRequestDto));
+    sendSignupVerificationUrl(user);
   }
 
   @Override
@@ -49,10 +41,8 @@ public class AuthServiceImpl implements AuthService {
     SignupVerification signupVerification = authDataHelpService
       .findOrCreateSVByUser(user);
     String newToken = generateAuthKeyService.GenerateSignupToken();
-      if (signupVerification.isVerified()) {
-        System.out.println("이미 인증처리까지 된 유저인데?");
+      if (signupVerification.isVerified())
           throw new AlreadyExistsUserException(user.getEmail());
-      }
       mailService.
         sendUserJoinVerificationMail(newToken, user.getName(), user.getEmail());
   }
--- server/src/main/java/spring/community/user/service/UserServiceImpl.java
+++ server/src/main/java/spring/community/user/service/UserServiceImpl.java
@@ -1,28 +1,33 @@
 package spring.community.user.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.community.authentication.dto.SignupRequestDto;
 import spring.community.user.entity.User;
 import spring.community.user.repository.dao.UserRepository;
+import spring.community.user.service.interfaces.UserService;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
 
-  public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
-
+  @Deprecated
   public Optional<User> findById(Long id) {
     return userRepository.findById(id);
   }
 
+  @Deprecated
   public List<User> findAll() {
      return userRepository.findAll();
   }
 
+  @Override
+  public User createUserByRequestDto(SignupRequestDto dto) {
+    return userRepository.save(dto.toUserEntity());
+  }
 }
