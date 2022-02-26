8328c79881732b57eb7fc9e94be52443e4ac0930
galaxy <galaxyhi4276@gmail.com>
2022-02-26 09:44:53 +0000

    refactor: lombok 생성자 방식으로 수정

--- server/src/main/java/spring/community/user/controller/UserController.java
+++ server/src/main/java/spring/community/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package spring.community.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,13 +17,9 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("userTest")
+@AllArgsConstructor
 public class UserController {
   private final UserServiceImpl userService;
-
-  public UserController(UserServiceImpl userService) {
-    this.userService = userService;
-  }
-
 
   @Operation(summary = "유저 id로 찾기")
   @GetMapping(value = "/{userId}")
