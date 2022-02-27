c201648792f93f002b21cebb05d3d4f13554e2f7
galaxy <galaxyhi4276@gmail.com>
2022-02-27 11:46:42 +0000

    feat: 공용 예외 클래스, 로직 작성

--- server/src/main/java/spring/community/exception/ErrorCode.java
+++ server/src/main/java/spring/community/exception/ErrorCode.java
@@ -7,6 +7,8 @@ public enum ErrorCode {
 
   INVALID_INPUT_VALUE("IIV01", "입력 값이 잘못되었습니다."),
   ALREADY_EXISTS_TARGET("AET01", "이미 존재하는 대상입니다."),
+  NOT_FOUND_TARGET("NFT01", "존재하지 않는 값 입니다."),
+  INVALID_TARGET("ET01", "대상이 만료되었습니다."),
   ;
 
   private String code;
--- server/src/main/java/spring/community/exception/GlobalExceptionHandler.java
+++ server/src/main/java/spring/community/exception/GlobalExceptionHandler.java
@@ -1,13 +1,14 @@
 package spring.community.exception;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler {
 
   /**
@@ -17,9 +18,31 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ErrorResponse> handleValidationException(
     MethodArgumentNotValidException ex
   ) {
-    log.error("handleValidationExceptions", ex);
     final ErrorResponse errResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, ex.getBindingResult());
     return ResponseEntity.badRequest().body(errResponse);
+  }
+
+  @ExceptionHandler(NotFoundTargetException.class)
+  public ResponseEntity<ErrorResponse> handleNotFoundException(
+    NotFoundTargetException ex
+  ) {
+    final ErrorResponse errorResponse = ErrorResponse.of(ex.getErrorCode());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+  }
+
+  @ExceptionHandler(InvalidValueException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidValueException(
+    InvalidValueException ex
+  ) {
+    List<ErrorResponse.FieldError> errors =
+      ErrorResponse.FieldError.of(ex.getField(), ex.getValue(), ex.getMessage());
+
+    final ErrorResponse errorResponse = ErrorResponse.of(
+      ex.getValue(),
+      ex.getErrorCode(),
+      errors
+      );
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
   }
 
 }
